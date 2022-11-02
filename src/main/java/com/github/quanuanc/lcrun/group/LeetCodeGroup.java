d8927733dbf3c2692653303d6626dcfb5fccfdeb
Quanuanc <quanuanc@outlook.com>
2022-11-03 00:11:58 +0800

    fix: Can not track actionId

--- src/main/java/com/github/quanuanc/lcrun/group/LeetCodeGroup.java
+++ src/main/java/com/github/quanuanc/lcrun/group/LeetCodeGroup.java
@@ -1,10 +1,7 @@
 package com.github.quanuanc.lcrun.group;
 
-import com.github.quanuanc.lcrun.action.GetCode;
-import com.github.quanuanc.lcrun.action.RunCode;
-import com.github.quanuanc.lcrun.action.Submit;
-import com.github.quanuanc.lcrun.icons.LCRunIcons;
 import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
@@ -14,10 +11,11 @@ public class LeetCodeGroup extends ActionGroup {
 
     @Override
     public AnAction @NotNull [] getChildren(@Nullable AnActionEvent e) {
+        ActionManager manager = ActionManager.getInstance();
         return new AnAction[]{
-                new GetCode("Get Code", "Get code", LCRunIcons.GET_ICON),
-                new RunCode("Run Code", "Run code", LCRunIcons.RUN_ICON),
-                new Submit("Submit", "Submit code", LCRunIcons.SUBMIT_ICON),
+                manager.getAction("LCRun.GetCode"),
+                manager.getAction("LCRun.RunCode"),
+                manager.getAction("LCRun.Submit")
         };
     }
 }
